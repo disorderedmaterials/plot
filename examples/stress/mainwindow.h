54aa58bef7e38827333841f2f6234af900fb762f
Tristan Youngs <tristan.youngs@stfc.ac.uk>
2022-04-21 14:15:13 +0100

    Add stress test example.

---
+++ examples/stress/mainwindow.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#include "ui_mainwindow.h"
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+    public:
+    MainWindow();
+    ~MainWindow() = default;
+
+    /*
+     * UI
+     */
+    private:
+    // Main form declaration
+    Ui::MainWindow ui_;
+
+    /*
+     * Data
+     */
+    private:
+    // Test data
+    std::vector<double> squaresX_, squaresValues_;
+};
