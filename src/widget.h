333e8f0548189f4acc910fd9928377f420c212e4
Tristan Youngs <tristan.youngs@stfc.ac.uk>
2022-04-22 11:00:45 +0100

    Break scenegraph out to separate file.

--- src/widget.h
+++ src/widget.h
@@ -135,6 +135,12 @@ class MildredWidget : public QWidget
     void updateShaderParameters();
     // Reset view
     void resetView();
+    // Axis Titles
+    void setXAxisTitle(const QString &title);
+    void setYAxisTitle(const QString &title);
+    void setZAxisTitle(const QString &title);
+    // Debug Objects
+    void setSceneCuboidEnabled(bool enabled);
 
     /*
      * Mouse Handling / Interaction
@@ -166,16 +172,5 @@ class MildredWidget : public QWidget
     public:
     // Add new data entity for supplied data
     Data1DEntity *addData1D(std::string_view tag);
-
-    /*
-     * Slots
-     */
-    public slots:
-    // Axis Titles
-    void setXAxisTitle(const QString &title);
-    void setYAxisTitle(const QString &title);
-    void setZAxisTitle(const QString &title);
-    // Debug Objects
-    void setSceneCuboidEnabled(bool enabled);
 };
 } // namespace Mildred
