f90210bb80cbe8a8ad99088184ed1745b5bfedbe
Jared Swift <jared.swift@stfc.ac.uk>
2022-05-13 13:30:26 +0100

    Updated example to offer uniform/random errors.

--- examples/errors/mainwindow.h
+++ examples/errors/mainwindow.h
@@ -1,7 +1,7 @@
 #pragma once
 
 #include "ui_mainwindow.h"
-
+#include "entities/data1d.h"
 class MainWindow : public QMainWindow
 {
     Q_OBJECT
@@ -13,14 +13,20 @@ class MainWindow : public QMainWindow
     /*
      * UI
      */
+    private slots:
+    void on_UniformErrorRadio_clicked(bool checked);
+    void on_RandomErrorRadio_clicked(bool checked);
+
     private:
     // Main form declaration
     Ui::MainWindow ui_;
+    // Display group
+    Mildred::Data1DEntity *dataEntity_{nullptr};
 
     /*
      * Data
      */
     private:
     // Test data
-    std::vector<double> squaresX_, squaresValues_, squareErrors_;
+    std::vector<double> xValues_, yValues_, uniformErrors_, randomErrors_;
 };
--- examples/errors/mainwindow_funcs.cpp
+++ examples/errors/mainwindow_funcs.cpp
@@ -1,38 +1,52 @@
 #include "mainwindow.h"
 #include "material.h"
 #include "ui_mainwindow.h"
+#include <random>
+
 
 MainWindow::MainWindow() : QMainWindow()
 {
     ui_.setupUi(this);
 
-    // View
-    connect(ui_.ResetViewButton, SIGNAL(clicked(bool)), ui_.TestingWidget, SLOT(resetView()));
-    connect(ui_.ShowSceneCuboidCheck, SIGNAL(clicked(bool)), ui_.TestingWidget, SLOT(setSceneCuboidEnabled(bool)));
-    ui_.FlatViewCheck->setChecked(ui_.TestingWidget->isFlatView());
-    connect(ui_.FlatViewCheck, SIGNAL(clicked(bool)), ui_.TestingWidget, SLOT(setFlatView(bool)));
-
-    // Axes
-    ui_.XTitleEdit->setText(ui_.TestingWidget->xAxis()->titleText());
-    connect(ui_.XTitleEdit, SIGNAL(textChanged(const QString &)), ui_.TestingWidget, SLOT(setXAxisTitle(const QString &)));
-    ui_.YTitleEdit->setText(ui_.TestingWidget->yAxis()->titleText());
-    connect(ui_.YTitleEdit, SIGNAL(textChanged(const QString &)), ui_.TestingWidget, SLOT(setYAxisTitle(const QString &)));
-    ui_.ZTitleEdit->setText(ui_.TestingWidget->zAxis()->titleText());
-    connect(ui_.ZTitleEdit, SIGNAL(textChanged(const QString &)), ui_.TestingWidget, SLOT(setZAxisTitle(const QString &)));
-    ui_.XVisibleCheck->setChecked(ui_.TestingWidget->xAxis()->isEnabled());
-    connect(ui_.XVisibleCheck, SIGNAL(toggled(bool)), ui_.TestingWidget->xAxis(), SLOT(setEnabled(bool)));
-    ui_.YVisibleCheck->setChecked(ui_.TestingWidget->yAxis()->isEnabled());
-    connect(ui_.YVisibleCheck, SIGNAL(toggled(bool)), ui_.TestingWidget->yAxis(), SLOT(setEnabled(bool)));
-    ui_.ZVisibleCheck->setChecked(ui_.TestingWidget->zAxis()->isEnabled());
-    connect(ui_.ZVisibleCheck, SIGNAL(toggled(bool)), ui_.TestingWidget->zAxis(), SLOT(setEnabled(bool)));
+    // Initialise a random engine
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_real_distribution<> dist(0.0, 1.0);
 
     // Data
-    for (auto n = -5; n <= 5; ++n)
-    {
-        squaresX_.push_back(n);
-        squaresValues_.push_back(fabs(n));
-        squareErrors_.push_back(0.1 * fabs(n));
-    }
-    auto *squares = ui_.TestingWidget->addData1D("Squares");
-    squares->setData(squaresX_, squaresValues_, squareErrors_);
+    const auto nPoints = 10;
+    double relativeError = 0.1;
+
+    // Fill in x values (0 to nPoints).
+    xValues_ = std::vector<double>(nPoints);
+    std::iota(xValues_.begin(), xValues_.end(), 0);
+
+    // Fill in y values (sin(x)).
+    yValues_ = std::vector<double>(nPoints);
+    std::transform(xValues_.begin(), xValues_.end(), yValues_.begin(), [](const auto x) {return sin(x);});
+    
+    // Fill in uniform errors.
+    uniformErrors_ = std::vector<double>(nPoints);
+    std::transform(yValues_.begin(), yValues_.end(), uniformErrors_.begin(), [relativeError](const auto x) {return relativeError * x;});
+    
+    // Fill in random errors.
+    randomErrors_ = std::vector<double>(nPoints);
+    std::transform(yValues_.begin(), yValues_.end(), randomErrors_.begin(), [dist, gen](const auto x) mutable {return x * dist(gen);});
+
+    dataEntity_ = ui_.TestingWidget->addData1D("Sin");
+    dataEntity_->setData(xValues_, yValues_, uniformErrors_);
+
 };
+
+void MainWindow::on_UniformErrorRadio_clicked(bool checked) {
+    if (checked) {
+        dataEntity_->setData(xValues_, yValues_, uniformErrors_);
+    }
+
+}
+
+void MainWindow::on_RandomErrorRadio_clicked(bool checked) {
+    if (checked) {
+        dataEntity_->setData(xValues_, yValues_, randomErrors_);
+    }
+}
