ce666a30a043aaeb2c66dc6e36b62755e7501f6d
Tristan Youngs <tristan.youngs@stfc.ac.uk>
2023-06-26 12:25:57 +0100

    Update flake (#64)
    * Update singularity generation.
    
    * Use nixGL.
    
    * Move to LLVM 13 (nix).

--- flake.nix
+++ flake.nix
@@ -4,9 +4,11 @@
     future.url = "github:NixOS/nixpkgs/nixos-unstable";
     outdated.url = "github:NixOS/nixpkgs/nixos-21.05";
     qt-idaaas.url = "github:disorderedmaterials/qt-idaaas";
+    nixGL-src.url = "github:guibou/nixGL";
+    nixGL-src.flake = false;
   };
   outputs =
-    { self, nixpkgs, future, outdated, flake-utils, bundlers, qt-idaaas }:
+    { self, nixpkgs, future, outdated, flake-utils, bundlers, qt-idaaas, nixGL-src }:
     let
 
       version = "0.1";
@@ -30,6 +32,7 @@
     let
       pkgs = import nixpkgs { inherit system; };
       next = import future { inherit system; };
+      nixGL = import nixGL-src { inherit pkgs; };
       qt = qt-idaaas.packages.${system};
     in
     {
@@ -38,7 +41,7 @@
         buildInputs = base_libs pkgs ++ gui_libs {inherit pkgs; q=qt;} ++ check_libs pkgs
           ++ (with pkgs; [
           (pkgs.clang-tools.override {
-            llvmPackages = pkgs.llvmPackages_7;
+            llvmPackages = pkgs.llvmPackages_13;
           })
           ccache
           ccls
@@ -73,10 +76,10 @@
           buildInputs = base_libs pkgs ++ (gui_libs {inherit pkgs; q=qt;});
           nativeBuildInputs = [ pkgs.wrapGAppsHook ];
 
-          cmakeFlags = [ "-G Ninja" ];
+          cmakeFlags = [ "-G Ninja -DBUILD_EXAMPLES:bool=true"];
           installPhase = ''
             mkdir -p $out/bin
-            mv bin/* $out/bin/
+            mv ./$out/bin/groups $out/bin/
           '';
 
           meta = with pkgs.lib; {
@@ -91,10 +94,11 @@
           nixpkgs.legacyPackages.${system}.singularity-tools.buildImage {
             name = "mildred-${version}";
             diskSize = 1024 * 250;
+            memSize = 1024 * 2;
             contents = [
               self.packages.${system}.library
             ];
-            runScript = "${self.packages.${system}.library}/bin/mildred $@";
+            runScript = "${nixGL.nixGLIntel}/bin/nixGLIntel ${self.packages.${system}.library}/bin/groups $@";
           };
       };
     });
