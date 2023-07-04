87c4f51d8292fb570645296edb054f6ac052a042
Pranavya-Vivek <135618736+Pranavya-Vivek@users.noreply.github.com>
2023-07-04 13:40:04 +0100

    Update base.h

--- src/renderers/1d/base.h
+++ src/renderers/1d/base.h
@@ -71,4 +71,42 @@ class ErrorRenderer1D
     void setErrorBarMetric(double errorBarMetric);
 };
 
+//! SymbolRenderer1DBase is the base class for all 1-dimensional symbol renderers.
+/*!
+ * SymbolRenderer1DBase provides a base class for all 1-dimensional symbol rendering styles.
+ */
+class SymbolRenderer1D
+{
+    public:
+    SymbolRenderer1D(Qt3DCore::QEntity *rootEntity) : rootEntity_(rootEntity){};
+    virtual ~SymbolRenderer1D() = default;
+
+    /*
+     * Entities
+     */
+    protected:
+    // Root Entity
+    Qt3DCore::QEntity *rootEntity_{nullptr};
+
+    /*
+     * Rendering
+     */
+    protected:
+    // Colour definition
+    ColourDefinition colour_;
+
+    // Symbol metrics
+    double symbolMetric_{6.0};
+
+    public:
+    // Create entities from the supplied axes and data
+    virtual void create(const ColourDefinition &colour, const std::vector<double> &x, const AxisEntity *xAxis,
+                        const std::vector<double> &values, const std::vector<double> &symbols, const AxisEntity *valueAxis) =0;
+    // Get symbol metric.
+    double symbolMetric() const;
+    // Set symbol metric.
+    void setSymbolMetric(double symbolMetric);
+
+};
+
 } // namespace Mildred
