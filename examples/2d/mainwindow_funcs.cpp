9aefb9927be91280d1823a8446ba741b91f2e5f1
Pranavya Vivekanand <135618736+Pranavya-Vivek@users.noreply.github.com>
2023-08-15 12:34:28 +0100

    Create mainwindow_funcs.cpp

---
+++ examples/2d/mainwindow_funcs.cpp
@@ -0,0 +1,51 @@
+#include "mainwindow.h"
+#include "material.h"
+#include "ui_mainwindow.h"
+
+MainWindow::MainWindow() : QMainWindow()
+{
+    ui_.setupUi(this);
+
+    generateFunction(Test2DFunctions::AckleyFunction);
+    dataEntity_ = ui_.TestingWidget->addData2D("2D Function");
+    dataEntity_->setData(x_, y_, values_);
+
+    ui_.TestingWidget->zAxis()->setEnabled(true);
+    ui_.TestingWidget->zAxis()->setType(Mildred::AxisEntity::AxisType::Vertical);
+    //ui_.TestingWidget->yAxis()->setType(Mildred::AxisEntity::AxisType::Depth);
+
+    ui_.TestingWidget->showAllData();
+};
+
+// Generate specified function
+void MainWindow::generateFunction(Test2DFunctions function)
+{
+    // Initialise the data and calculate the delta
+    // Data, all bounded to [-5,5] in x and y
+    const auto axisMin = -50.0, axisMax = 50.0, delta = (axisMax - axisMin) / (nPoints_ - 1);
+
+    x_.reserve(nPoints_);
+    x_.clear();
+    values_.reserve(nPoints_ * nPoints_);
+    values_.clear();
+
+    // Generate x axis and duplicate as y
+    for (auto n = 0; n <= nPoints_; ++n)
+    x_.push_back(n * delta);
+    y_ = x_;
+
+    switch (function)
+    {
+        case (Test2DFunctions::AckleyFunction):
+            /*
+             * Ackley's Function
+             * f(x,y) = -20.0 * exp(-0.2 * sqrt(0.5 * (x^2 + y^2))) - exp(0.5 * (cos(2 * M_PI * x) + cos(2 * M_PI * y))) + e +
+             * 20
+             */
+            for (const auto x : x_)
+                for (const auto y : y_)
+                    values_.push_back(-20.0 * exp(-0.2 * sqrt(0.5 * (x * x + y * y))) -
+                                      exp(0.5 * (cos(2 * M_PI * x) + cos(2 * M_PI * y))) + M_E + 20);
+            break;
+    }
+}
