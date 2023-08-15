fc35897ac51834daae33320e1edd022309b084c7
Pranavya Vivekanand <135618736+Pranavya-Vivek@users.noreply.github.com>
2023-08-15 12:52:11 +0100

    Update data3d.cpp

--- src/entities/data3d.cpp
+++ src/entities/data3d.cpp
@@ -1,4 +1,5 @@
 #include "entities/data3d.h"
+#include "renderers/3d/stylefactory.h"
 
 using namespace Mildred;
 
@@ -6,10 +7,14 @@ using namespace Mildred;
 /*!
  * Construct a new Data3DEntity storing a reference to the supplied @param metrics and with the given @param parent.
  */
-Data3DEntity::Data3DEntity(MildredMetrics &metrics, Qt3DCore::QNode *parent) : DataEntity(parent) {}
-
+Data3DEntity::Data3DEntity(const AxisEntity *xAxis, const AxisEntity* yAxis, const AxisEntity* zAxis, const AxisEntity *valueAxis, Qt3DCore::QNode *parent,
+                           StyleFactory3D::Style style)
+    : DataEntity(parent), xAxis_(xAxis), yAxis_(yAxis), zAxis_(zAxis), valueAxis_(valueAxis), style_(style)
+{
+    dataRenderer_ = StyleFactory3D::createDataRenderer(style_, dataEntity_);
+}
 /*
- * Data
+ * Data 
  */
 
 //! Clear all data vectors
@@ -25,17 +30,42 @@ void Data3DEntity::clearData()
 
 //! Set display data
 /*!
- * Set the supplied three-dimensional data (axis points @param x, @param y, and @param z and @param values at those points). The
- * data are copied to local arrays and entities representing the data in the current style are immediately created.
+ * Set the supplied two-dimensional data (axis points @param x and @param y and @param z and @param values at those points). The data are
+ * copied to local arrays and entities representing the data in the current style are immediately created.
  */
 void Data3DEntity::setData(std::vector<double> x, std::vector<double> y, std::vector<double> z, std::vector<double> values)
 {
     clearData();
 
-    x_ = std::move(x);
-    y_ = std::move(y);
-    z_ = std::move(z);
-    values_ = std::move(values);
+    if (x.size() * y.size() * z.size()!= values.size())
+        printf("Irregular vector sizes provided (%zu (x) vs %zu (y) vs %zu (z) vs %zu (values)) so data will be ignored.\n", x.size(), y.size(), z.size(), values.size());
+    else
+    {
+        x_ = std::move(x);
+        y_ = std::move(y);
+        z_ = std::move(z);
+        values_ = std::move(values);
+    }
 
-    //    create();
+    // Determine axis extrema
+    auto vit = values_.cbegin();
+    for (const auto x : x_)
+        for (const auto y : y_)
+        {
+        updateExtrema(x, y, *vit);
+        ++vit;
+    }
+
+    create();
+}
+
+/*
+ * Rendering
+ */
+
+//! Create renderables in the current style
+void Data3DEntity::create()
+{
+    assert(dataRenderer_);
+    dataRenderer_->create(colourDefinition(), x_, xAxis_, y_, yAxis_, z_, zAxis_, values_, valueAxis_);
 }
